b85310ea702cbd436d2071d0615c8ab544534658
Md Mahbub Reza <141657720+mdmahbubreza@users.noreply.github.com>
2025-03-17 17:37:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PyPDF2 import PdfReader
 import streamlit.components.v1 as components
 
 # Define the API key directly in the code
-API_KEY = "PASTE API KEY"
+API_KEY = "PASTE API KEY" ##api key!!!!!!
 
 def configure_genai():
     """Configure the Gemini AI with the API key."""
